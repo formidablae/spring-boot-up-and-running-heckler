24a565d7e1bdd150d26065210d7c328a298df6f0
formidablae <81068781+formidablae@users.noreply.github.com>
2023-02-04 12:56:04 +0100

    Created sbur-rest-demo project

---
+++ 03_Creating_Your_First_Spring_Boot_REST_API/sbur-rest-demo/src/main/java/com/thehecklers/sburrestdemo/SburRestDemoApplication.java
@@ -0,0 +1,13 @@
+package com.thehecklers.sburrestdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SburRestDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SburRestDemoApplication.class, args);
+	}
+
+}
